971207a799afab8b6ac88a7e41a52efb5dcfd654
Mafiameower <Quintendoty@hotmail.com>
2024-01-05 22:01:10 -0600

    Update and tested Invoke_Mg_User.ps1
    Added comment tags to get-mguser-invoke.
    Added more parameters to update-MgUser-Invoke for multiple updates at once.
    Added functionality of new parameters

--- Mg-Graph_Modules/Invoke_mg_User.ps1
+++ Mg-Graph_Modules/Invoke_mg_User.ps1
@@ -11,14 +11,17 @@ function Get-MgUser-Invoke() {
     param([string]$Mail, [string]$DisplayName, [string]$CustomParameter, [switch]$nocls, [switch]$less, [string]$find)
     #Get UserID for user
     if ($Mail) {
+    #Use -mail to search for user using entire Email address.
         $user = invoke-mggraphrequest -method GET -Uri "/v1.0/users?`$filter=mail eq '$Mail'" 
         $userID = $user.value.id
     }
     if ($DisplayName) {
+    #This search method uses StartsWith Displayname filter parameter. This will only work well if you have a user with a somewhat unique name.
         $user = invoke-mggraphrequest -method GET -Uri "/v1.0/users?`$filter=startsWith(displayName,'$displayName')" 
         $userID = $user.value.id
     }
     if ($find) {
+    #This is the best method to use if you have a user with a very common name. Then you can get anothere more unique identifier to locate them.
         $userList = invoke-mggraphrequest -method GET -Uri "/v1.0/users?`$filter=startsWith(displayName,'$find')?`$select=displayName, mail, id"
         $userList = $userList.value | Select-Object displayName, mail, id
         return $userList
@@ -53,17 +56,52 @@ function Get-MgUser-Invoke() {
 
 function Update-MgUser-Invoke() {
     param(
-        [Parameter(Mandatory)]
-        [string]$mail, [string]$parameter, [string]$newValue
+        [Parameter(Mandatory, Position=0)][string]$Mail,
+        [Parameter(Mandatory, Position=1)][string][string]$Parameter,
+        [Parameter(Mandatory, Position=2)][string][string]$newValue,
+        [Parameter(Position=3)][string]$Parameter2,
+        [Parameter(Position=4)][string]$newValue2,
+        [Parameter(Position=5)][string]$Parameter3,
+        [Parameter(Position=6)][string]$newValue3,
+        [Parameter(Position=7)][string]$Parameter4,
+        [Parameter(Position=8)][string]$newValue4
     )
-    $body = @{"$parameter" = "$newValue" }
+    $body = @{"$Parameter" = "$newValue"}
+    $body2 = @{"$Parameter2" = "$newValue2"}
+    $body3 = @{"$Parameter3" = "$newValue3"}
+    $body4 = @{"$Parameter4" = "$newValue4"}
     Write-Host "Before Update"
     Write-Host ":::::::::::::::::::"
-    Get-MgUser-Invoke $mail 
+    if($parameter2){
+        Get-MgUser-Invoke $mail -less
+        $userID = invoke-mggraphrequest -method GET  -Uri "/v1.0/users?`$filter=mail eq '$mail'" | Select-Object -ExpandProperty value | Select-Object -ExpandProperty id
+        Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body
+        Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body2
+        return Get-MgUser-Invoke $mail -less
+    }
+    if($parameter3){
+        Get-MgUser-Invoke $mail -less
+        $userID = invoke-mggraphrequest -method GET  -Uri "/v1.0/users?`$filter=mail eq '$mail'" | Select-Object -ExpandProperty value | Select-Object -ExpandProperty id
+        Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body
+        Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body2
+        Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body3
+        return Get-MgUser-Invoke $mail -less
+    }
+    if($parameter4){
+        Get-MgUser-Invoke $mail -less
+        $userID = invoke-mggraphrequest -method GET  -Uri "/v1.0/users?`$filter=mail eq '$mail'" | Select-Object -ExpandProperty value | Select-Object -ExpandProperty id
+        Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body
+        Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body2
+        Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body3
+        Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body4
+        return Get-MgUser-Invoke $mail -less
+    }
+
+    Get-MgUser-Invoke $mail -less
     $userID = invoke-mggraphrequest -method GET  -Uri "/v1.0/users?`$filter=mail eq '$mail'" | Select-Object -ExpandProperty value | Select-Object -ExpandProperty id
     Invoke-MgGraphRequest -Method PATCH -Uri "/v1.0/users/{$userID}" -Body $body
     Write-Host "Updated information"
     Write-Host ":::::::::::::::::::"
-    Get-MgUser-Invoke $mail
+    return Get-MgUser-Invoke $mail -less
 }
 
